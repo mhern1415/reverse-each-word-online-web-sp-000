6395c850244039b3c34210e4f217bff77ec74953
Mark Hernandez <m.hern1415@gmail.com>
2019-05-03 19:31:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 
-
 def reverse_each_word(array)
   new_array = array.split(" ")
-  reversed_array = new_array.each {|x| x.reverse!}
-  return reversed_array.join(" ")
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
 end
