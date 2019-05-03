75ebdce46d67fa7995c0eea23fe71e1604979fa2
Mark Hernandez <m.hern1415@gmail.com>
2019-05-03 19:31:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,9 @@ def reverse_each_word(sentence)
 	end
   .join(" ")
 end
+
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
+end
